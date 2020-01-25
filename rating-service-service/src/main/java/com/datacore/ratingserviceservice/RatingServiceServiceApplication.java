5f5b093fe39e41a9d220f2bc3717eaf2b91a6721
dimak <dimak@DESKTOP-LFJS8TF>
2020-01-25 17:55:07 +0200

    Set up all microservices as Eureka clients

--- rating-service-service/src/main/java/com/datacore/ratingserviceservice/RatingServiceServiceApplication.java
+++ rating-service-service/src/main/java/com/datacore/ratingserviceservice/RatingServiceServiceApplication.java
@@ -2,8 +2,10 @@ package com.datacore.ratingserviceservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class RatingServiceServiceApplication {
 
 	public static void main(String[] args) {
